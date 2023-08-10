3aed985a548bd56eb216abf0361df949a5f64e75
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-08-10 14:44:59 -0300

    delete

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -2,7 +2,6 @@ const env = {
 	port: process.env.PORT || 3000,
     databaseUrl: process.env.DATABASE_URL,
     secretKey: process.env.SECRET_KEY || 'secret',
-
     stipeSecretKey: process.env.STRIPE_SECRET_KEY
 
 }
